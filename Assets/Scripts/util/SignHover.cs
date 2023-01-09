55806606b1c1ffc55c28ed2692577ee7a439b39a
Patrick Herbers <patrick.herbers@rub.de>
2023-01-09 17:35:56 +0100

    Tutorial level

--- Assets/Scripts/util/SignHover.cs
+++ Assets/Scripts/util/SignHover.cs
@@ -7,21 +7,22 @@ public class SignHover : MonoBehaviour
     public float speed = 0.69f;
     public float magnitude = 0.25f;
     private float dt;
+    private float x, y, z;
 
     // Start is called before the first frame update
     void Start()
     {
         //Random jitter
         dt = Random.Range(0.0f, 200.0f);
+        
+        x = transform.position.x;
+        y = transform.position.y;
+        z = transform.position.z;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float x = transform.position.x;
-        float y = transform.position.y;
-        float z = transform.position.z;
-
         dt += Time.deltaTime * speed;
         Vector3 pos = new Vector3(x, y + MathF.Sin(dt) * magnitude, z);
         transform.position = pos;
